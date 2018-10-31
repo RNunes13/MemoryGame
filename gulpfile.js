e51819b931f6bb2668b475027236a846f84e3c15
Unknown <nunesrodrigo13@outlook.com>
2018-10-31 11:20:37 -0300

    Adjustments - Modifying the gulp file structure
    - Added an environment check;
    - Added an error message when there is an error in the execution of the watch;
    - Removed vendor tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,36 @@ const clean = require('gulp-clean');
 const cssmin = require('gulp-cssmin');
 const imagemin = require('gulp-image');
 const uglify = require('gulp-uglify');
+const notify = require('gulp-notify');
 const runSequence = require('run-sequence');
 const browserify = require('gulp-browserify');
+const gulpif = require('gulp-if');
+const minimist = require('minimist');
 
 const SRC = './src';
 const DIST = './dist';
 const assetsSRC = `${SRC}/assets`;
 const assetsDIST = `${DIST}/assets`;
 const viewsSRC = `${SRC}/views`;
+
+const args = {
+  string: 'env',
+  default: {env: process.env.NODE_ENV || 'development'}, // 'production'
+};
+
+const options = minimist(process.argv.slice(2), args);
+
+const utils = {
+  error: function(error) {
+    console.log(error.toString());
+    utils.alert(error.toString());
+    this.emit('end');
+  },
+
+  alert: function(msg) {
+    gulp.src('./src').pipe(notify(msg));
+  },
+};
 
 const task = {
 
@@ -26,12 +48,14 @@ const task = {
   pug: function() {
     return gulp.src([`${viewsSRC}/*.pug`, `${viewsSRC}/_pages/*.pug`])
       .pipe(pug({pretty: true}))
+      .on('error', utils.error)
       .pipe(gulp.dest(`${DIST}`));
   },
 
   sass: function() {
     return gulp.src(`${assetsSRC}/scss/**/*.scss`)
       .pipe(sass())
+      .on('error', utils.error)
       .pipe(postcss([autoprefixer()]))
       .pipe(cssmin())
       .pipe(gulp.dest(`${assetsDIST}/css`));
@@ -41,6 +65,7 @@ const task = {
     return gulp.src([`${assetsSRC}/js/**/*.js`, `!${assetsSRC}/js/common/**/*.js`, `!${assetsSRC}/js/modules/**/*.js`])
       .pipe(browserify({transform: ['babelify']}))
       .pipe(uglify())
+      .on('error', utils.error)
       .pipe(gulp.dest(`${assetsDIST}/js`));
   },
 
@@ -48,39 +73,21 @@ const task = {
     return gulp.src(`${assetsSRC}/js/common/memory_game-common-globals.js`)
       .pipe(browserify({transform: ['babelify']}))
       .pipe(uglify())
+      .on('error', utils.error)
       .pipe(gulp.dest(`${assetsDIST}/js/common`));
   },
 
   img: function() {
     return gulp.src(`${assetsSRC}/img/**/*`)
-      .pipe(imagemin())
+      .pipe(gulpif(options.env === 'production', imagemin()))
+      .on('error', utils.error)
       .pipe(gulp.dest(`${assetsDIST}/img`));
-  },
-
-  vendor: {
-    css: function() {
-      return gulp.src(['node_modules/@fortawesome/fontawesome-free/css/all.min.css'])
-      .pipe(gulp.dest(`${assetsDIST}/css/vendor/fontawesome/css`));
-    },
-
-    webfonts: function() {
-      return gulp.src(['node_modules/@fortawesome/fontawesome-free/webfonts/*.*'])
-      .pipe(gulp.dest(`${assetsDIST}/css/vendor/fontawesome/webfonts`));
-    },
-
-    js: function() {
-      return gulp.src(['node_modules/jquery/dist/jquery.min.js'])
-      .pipe(gulp.dest(`${assetsDIST}/js/vendor`));
-    },
   },
 };
 
 gulp.task('clean', task.clean);
 gulp.task('pug', task.pug);
 gulp.task('sass', task.sass);
-gulp.task('cssVendor', task.vendor.css);
-gulp.task('jsVendor', task.vendor.js);
-gulp.task('webfontsVendor', task.vendor.webfonts);
 gulp.task('js', task.js);
 gulp.task('jsGlobal', task.jsGlobal);
 gulp.task('img', task.img);
@@ -105,5 +112,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', function(callback) {
-  runSequence('clean', ['pug', 'sass', 'js', 'jsGlobal', 'cssVendor', 'webfontsVendor', 'jsVendor', 'img'], callback);
+  runSequence('clean', ['pug', 'sass', 'js', 'jsGlobal', 'img'], callback);
 });
